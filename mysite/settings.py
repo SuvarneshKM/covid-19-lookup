0999d23df9e4b5f888e3381d41b8863da005b810
Suvarnesh K M <suvarnesh1729@gmail.com>
2020-05-05 23:50:37 +0530

    Commit Message

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,12 +81,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
     }
 }
 
-
+DATABASES['default'] = dj_database_url.config(default='postgres://dzgwagtcmavmmj:5ee71c08623c9d1405850be5d320eebded87f9c24d173138699abf6b6a414550@ec2-34-233-186-251.compute-1.amazonaws.com:5432/dd4jgh65rr7nci')
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
